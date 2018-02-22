f084c986da0de1ec583f33b4401edec22c311f9d
Amr Ali <devamrali@gmail.com>
2018-02-22 12:37:29 +0200

    initial

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var HelloWorld = /** @class */ (function () {
+    function HelloWorld(message) {
+        this.message = message;
+    }
+    return HelloWorld;
+}());
+var hello = new HelloWorld("Hello Typescript!");
+console.log(hello.message);
+//# sourceMappingURL=app.js.map
